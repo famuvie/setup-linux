588a052a99e0a5a4de9e8479c0ad7ba67b5748ed
Facundo Muñoz <facundo.munoz@cirad.fr>
2017-10-25 14:54:39 +0200

    general update

--- fmlt.sh
+++ fmlt.sh
@@ -4,11 +4,19 @@
 
 sh main.sh
 
+## ssh key pair
+if [ ! -e ~/.ssh/id_rsa.pub ]
+then
+	ssh-keygen -t rsa -C "facundo.munoz@cirad.fr" -b 4096
+fi
+
 ## Dotfiles
+# paste the contents of ~/.ssh/id_rsa.pub into https://gitlab.com/profile/keys
 git clone git@gitlab.com:famuvie/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 git checkout fmlt
-make stow-all
+make stow-all    # update all dotfiles with local versions (if there are) and make links
+git checkout -- .  # reset to dotfile versions in the repo
 cd
 
 ### Restore personal settings from backup ###
@@ -39,6 +47,13 @@ rsync -azv --progress 192.168.0.2:/media/facu/Toshiba1T/VirtualBox\ VMs ~
 #cp -r $bkploc/bin ~
 
 
+## Better off, if $bkploc contains .thunderbird, .mozilla, lib and VirtualBox\ VMs
+## just copy everything with
+## the trailing slash is necessary to copy the hidden directories
+## in part. do not use a * at the end, as it does not match .dirname
+rsync -azv $bkploc/ ~
+
+
 ### Software not installed in main.sh ###
 
 
@@ -49,3 +64,7 @@ sudo umount mnt
 ## Restore Work folder from fmhm sync
 mkdir Work
 unison fmhm 
+
+## Setup hamster's time-tracking database
+cd Work/logistica/Cirad/time-tracking
+make
--- main.sh
+++ main.sh
@@ -46,7 +46,8 @@ fi
 
 # GIS software (Ubuntugis)
 # unstable, because of a problem between GRASS and wxpython
-sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+# currently installing GIS software from official repositories. No need for more.
+# sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 
 # R-project
 sudo bash -c "echo 'deb http://cran.r-project.org/bin/linux/ubuntu' $ubuntu_codename'/' > /etc/apt/sources.list.d/cran-r-ppa-$ubuntu_codename.list"
@@ -105,7 +106,7 @@ fi
 # Warning unattended installation of all these with aptitude broke my cinnamon installation (some incompatibility in dependencies)
 # However, it seems not to happen with apt-get, so it looks safer for the moment.
 # As an alternative, it might be useful to use aptitude-robot
-sudo apt-get install apt-transport-https freemind guake gnome-do gnome-do-plugins htop keepass2 unison unison-gtk gftp meld playonlinux virtualbox virtualbox-qt umbrello pdftk recode ssh sshfs gtg okular audacity pdfshuffler pandoc xdotool xournal ispell xclip git-all timeshift stow zsh
+sudo apt-get install apt-transport-https freemind guake gnome-do gnome-do-plugins gparted hamster-indicator htop keepass2 unison unison-gtk gftp meld playonlinux virtualbox virtualbox-qt umbrello pdftk recode ssh sshfs gtg okular audacity pdfshuffler pandoc xdotool xournal ispell xclip git-all timeshift tmux stow zsh
 
 # missing: skype
 
@@ -117,6 +118,7 @@ sudo aptitude install sublime-text
 sudo aptitude -r install oracle-java8-installer
 
 # Calibre e-book manager (latest binary installation from webpage)
+# better install from official repo
 #sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
 sudo aptitude -r install calibre
 
@@ -142,18 +144,20 @@ fi
 #  - only english and spanish documentation
 #  - make symbolic links in system directories
 # It takes a while (some 40 minutes)
-wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-tar -xf install-tl-unx.tar.gz
-rm install-tl-unx.tar.gz
-cd install-tl-*
+# Easier to install texlive-full from official repos
+#wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+#tar -xf install-tl-unx.tar.gz
+#rm install-tl-unx.tar.gz
+#cd install-tl-*
 #sudo aptitude -ry install perl-tk # only needed for a gui install
-sudo ./install-tl -profile ../texlive.tlpdb
-tlversion=`grep -o 201. release-texlive.txt`
-sudo bash -c "echo -e '\n# LaTeX\nexport MANPATH=/usr/local/texlive/'$tlversion'/texmf-dist/doc/man/:\$MANPATH\nexport INFOPATH=/usr/local/texlive/'$tlversion'/texmf-dist/doc/info/:\$INFOPATH\nexport PATH=/usr/local/texlive/'$tlversion'/bin/'$arch'-linux/:\$PATH' >> /etc/profile"
-cd ..
-rm -r install-tl-*
+#sudo ./install-tl -profile ../texlive.tlpdb
+#tlversion=`grep -o 201. release-texlive.txt`
+#sudo bash -c "echo -e '\n# LaTeX\nexport MANPATH=/usr/local/texlive/'$tlversion'/texmf-dist/doc/man/:\$MANPATH\nexport INFOPATH=/usr/local/texlive/'$tlversion'/texmf-dist/doc/info/:\$INFOPATH\nexport PATH=/usr/local/texlive/'$tlversion'/bin/'$arch'-linux/:\$PATH' >> /etc/profile"
+#cd ..
+#rm -r install-tl-*
 # Still needed from repos:
-sudo aptitude install -ry tex-common texinfo lmodern perl-tk
+#sudo aptitude install -ry tex-common texinfo lmodern perl-tk
+sudo aptitude -ry install texlive-full perl-tk
 
 # Command for further updating in the future
 # sudo tlmgr update --all
@@ -172,69 +176,73 @@ case $arch in
 		rsarch='amd64'
 esac
 
-wget http://download1.rstudio.org/rstudio-$rsversion-$rsarch.deb
-sudo gdebi -n rstudio-$rsversion-i386.deb
-rm rstudio-$rsversion-i386.deb
+wget http://download1.rstudio.org/rstudio-$ubuntu_codename-$rsversion-$rsarch.deb
+sudo gdebi -n rstudio-$ubuntu_codename-$rsversion-i386.deb
+rm rstudio-$ubuntu_codename-$rsversion-i386.deb
 
 # Eclipse IDE + StatET
 # Follow instructions from 
 # http://www.stanford.edu/~messing/ComputationalSocialScienceWorkflow.html
 # Download the CDT version of Eclipse (with tools for C/C++)
-wget http://www.eclipse.org/downloads/
-eclipsemirror=`grep -m 1 -o www.*.cpp*tar.gz index.html`
-wget $eclipsemirror'&mirror_id=514'
-wget `grep -m 1 -o 'http.*tar.gz' download*`
-mv eclipse* ~/bin
-tar -xf ~/bin/eclipse*
-mv ~/bin/eclipse ~/bin/Eclipse
-ln -s ~/bin/Eclipse/eclipse ~/bin/eclipse
-# Until 2012 Eclipse needed the java 6 version from Sun
-# sudo aptitude -ry install sun-java6-jre sun-java6-jdk
-# From 2013, Sun java if Oracle's java with a more restricted license
-# so it can't be installed from repos. However, Eclipse seems too
-# work fine with Open Java 7, now.
-sudo aptitude -ry install default-jdk
-# StatET
-# Install manually from within Eclipse:
-# http://www.walware.de/goto/statet
-sudo R CMD javareconf
+# wget http://www.eclipse.org/downloads/
+# eclipsemirror=`grep -m 1 -o www.*.cpp*tar.gz index.html`
+# wget $eclipsemirror'&mirror_id=514'
+# wget `grep -m 1 -o 'http.*tar.gz' download*`
+# mv eclipse* ~/bin
+# tar -xf ~/bin/eclipse*
+# mv ~/bin/eclipse ~/bin/Eclipse
+# ln -s ~/bin/Eclipse/eclipse ~/bin/eclipse
+# # Until 2012 Eclipse needed the java 6 version from Sun
+# # sudo aptitude -ry install sun-java6-jre sun-java6-jdk
+# # From 2013, Sun java if Oracle's java with a more restricted license
+# # so it can't be installed from repos. However, Eclipse seems too
+# # work fine with Open Java 7, now.
+# sudo aptitude -ry install default-jdk
+# # StatET
+# # Install manually from within Eclipse:
+# # http://www.walware.de/goto/statet
+# sudo R CMD javareconf
 
 
-# gedit plugins
-# sudo aptitude -ry install gedit-developer-plugins gedit-plugins
-  # watch out! the latex plugin installs LaTeX!!
-  # We should install it beforehand (it does it anyway)
-  # It also installs Bazaar.
-  # gedit-developer-plugins requires gedit >= 3.2.0
-  # installing from source gave problems with libgedit.private.so.0 library not found
-  # Do not auto-accept the first solution, which is not installing developer plugins
-  # and take the second option, which is upgrading gedit
-  sudo aptitude install gedit-developer-plugins
+# Mint replaced gedit by xed as the default text editor
+# I could reinstall gedit, but I'm not using it so often as before
+# I will rely on Sublime Text/Atom as powerful text editors
+# and keep xed or whatever as is.
+# # gedit plugins
+# # sudo aptitude -ry install gedit-developer-plugins gedit-plugins
+#   # watch out! the latex plugin installs LaTeX!!
+#   # We should install it beforehand (it does it anyway)
+#   # It also installs Bazaar.
+#   # gedit-developer-plugins requires gedit >= 3.2.0
+#   # installing from source gave problems with libgedit.private.so.0 library not found
+#   # Do not auto-accept the first solution, which is not installing developer plugins
+#   # and take the second option, which is upgrading gedit
+#   sudo aptitude install gedit-developer-plugins
 
-# gedit-latex-plugin
-# In ubuntu 11.10 repos there is a version that works only with gedit-2.x
-# but Oneiric ships gedit-3.x. Need to install from
-#https://launchpad.net/ubuntu/oneiric/i386/gedit-latex-plugin/3.3.1-1~oneiric1
-#wget http://launchpadlibrarian.net/83566981/gedit-latex-plugin_3.3.1-1~oneiric1_all.deb
-#sudo gdebi -n gedit-latex-plugin_3.3.1-1~oneiric1_all.deb
-#rm gedit-latex-plugin_3.3.1-1~oneiric1_all.deb
-# This is solved now
+# # gedit-latex-plugin
+# # In ubuntu 11.10 repos there is a version that works only with gedit-2.x
+# # but Oneiric ships gedit-3.x. Need to install from
+# #https://launchpad.net/ubuntu/oneiric/i386/gedit-latex-plugin/3.3.1-1~oneiric1
+# #wget http://launchpadlibrarian.net/83566981/gedit-latex-plugin_3.3.1-1~oneiric1_all.deb
+# #sudo gdebi -n gedit-latex-plugin_3.3.1-1~oneiric1_all.deb
+# #rm gedit-latex-plugin_3.3.1-1~oneiric1_all.deb
+# # This is solved now
 
-# This plugin depends on rubber who in turn depends on texlive
-# It doesn't know that I have already installed TeX Live and
-# wants to install the outdated version from de repos.
-# Solution is to build a dummy package to cheat him
-# Source: http://blogs.ethz.ch/ubuntu/2011/03/14/tex-live-2010-installation/
-sudo apt-get install equivs
-mkdir tl-equivs && cp texlive-local tl-equivs && cd tl-equivs
-equivs-build texlive-local
-sudo dpkg -i texlive-local_2013-1~1_all.deb
-cd .. && rm -r tl-equivs
-# Finally:
-sudo aptitude install gedit-latex-plugin
+# # This plugin depends on rubber who in turn depends on texlive
+# # It doesn't know that I have already installed TeX Live and
+# # wants to install the outdated version from de repos.
+# # Solution is to build a dummy package to cheat him
+# # Source: http://blogs.ethz.ch/ubuntu/2011/03/14/tex-live-2010-installation/
+# sudo apt-get install equivs
+# mkdir tl-equivs && cp texlive-local tl-equivs && cd tl-equivs
+# equivs-build texlive-local
+# sudo dpkg -i texlive-local_2013-1~1_all.deb
+# cd .. && rm -r tl-equivs
+# # Finally:
+# sudo aptitude install gedit-latex-plugin
 
-# A workaround the latex-plugin who does not find texmf.cnf
-sudo ln -sf `kpsewhich -var-value TEXMFMAIN`/web2c/texmf.cnf /etc/texmf/texmf.cnf
+# # A workaround the latex-plugin who does not find texmf.cnf
+# sudo ln -sf `kpsewhich -var-value TEXMFMAIN`/web2c/texmf.cnf /etc/texmf/texmf.cnf
 
 
 # gedit-r-plugin
@@ -268,20 +276,17 @@ sudo ln -sf `kpsewhich -var-value TEXMFMAIN`/web2c/texmf.cnf /etc/texmf/texmf.cn
 # and using gconftool-2 as a CLI
 #gconftool-2 --set /apps/gedit-2/plugins/active-plugins [latex,changecase,filebrowser,docinfo,wordcompletion,time,terminal,externaltools,multiedit,modelines,bracketcompletion,synctex,codecomment,spell,textsize,RCtrl] --type=list --list-type=string
 
-# In Gnome 3.x the CLI is gsettings:
-gsettings set org.gnome.gedit.plugins active-plugins "['latex', 'changecase', 'filebrowser', 'docinfo', 'wordcompletion', 'time', 'terminal', 'externaltools', 'multiedit', 'modelines', 'bracketcompletion', 'synctex', 'codecomment', 'spell', 'textsize', 'RCtrl']"
+# # In Gnome 3.x the CLI is gsettings:
+# gsettings set org.gnome.gedit.plugins active-plugins "['latex', 'changecase', 'filebrowser', 'docinfo', 'wordcompletion', 'time', 'terminal', 'externaltools', 'multiedit', 'modelines', 'bracketcompletion', 'synctex', 'codecomment', 'spell', 'textsize', 'RCtrl']"
 
-# gedit preferences
-    # ancho del tabulador: 2
-    # usar espacios en lugar de tabuladores (seguro?)
-    # activar sangría automática
-gsettings set org.gnome.gedit.preferences.editor tabs-size 2
-gsettings set org.gnome.gedit.preferences.editor insert-spaces true
-gsettings set org.gnome.gedit.preferences.editor auto-indent true
+# # gedit preferences
+#     # ancho del tabulador: 2
+#     # usar espacios en lugar de tabuladores (seguro?)
+#     # activar sangría automática
+# gsettings set org.gnome.gedit.preferences.editor tabs-size 2
+# gsettings set org.gnome.gedit.preferences.editor insert-spaces true
+# gsettings set org.gnome.gedit.preferences.editor auto-indent true
 
-
-# Bazaar
-# sudo aptitude -ry install bzr-explorer bzr-svn
 
 # Geographical libraries GDAL and Proj4
 sudo aptitude -ry install libgdal-dev libproj-dev
@@ -289,10 +294,13 @@ sudo aptitude -ry install libgdal-dev libproj-dev
 
 # Gephi: Graph Viz interactive visualization
 # This is the latest release up to date.
-wget https://launchpad.net/gephi
-gephifile=`grep -m 1 -o http.*tar.gz gephi`
-rm gephi
+# Moved to GitHub
+# wget https://launchpad.net/gephi
+wget https://gephi.org/users/download/
+gephifile=`grep -m 1 -o http.*tar.gz index.html`
+rm index.html
 wget $gephifile
+mkdir -p ~/bin
 tar -C ~/bin -xf gephi-*
 rm gephi-*
 # TODO: put shortcut in Gnome menus
@@ -309,7 +317,7 @@ rm gephi-*
 
 # Dropbox
 # Latest version
-wget https://www.dropbox.com/install2
+wget https://www.dropbox.com/install
 case $arch in
 	i386)
 		dbarch=$arch;;
@@ -317,9 +325,9 @@ case $arch in
 		dbarch='amd64'
 esac
 
-dbsufix=`grep -o /download?dl=packages/ubuntu/dropbox[_\.0-9]*$dbarch.deb install2`
+dbsufix=`grep -o /download?dl=packages/ubuntu/dropbox[_\.0-9]*$dbarch.deb install`
 wget https://www.dropbox.com$dbsufix
-rm install2
+rm install
 sudo dpkg -i download*
 rm download*
 sudo aptitude -r install nemo-dropbox
@@ -343,13 +351,15 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 ## TODO:
 ## Configure guake to be run at the begining of the session and get the transparency right
 
-## Default shell
-chsh -s /bin/zsh
+# ## Default shell
+# Done during installation
+# chsh -s /bin/zsh
 
 ## alias ll = ls -l for Mint
-if [ $mint ];
-    then echo -e "alias ll='ls -Flh --group-directories-first'\nalias la='ll -a'" >> ~/.bashrc;
-fi
+# handled with dotfiles
+# if [ $mint ];
+#     then echo -e "alias ll='ls -Flh --group-directories-first'\nalias la='ll -a'" >> ~/.bashrc;
+# fi
 
 ## Git config
 ## handled in the dotfile
