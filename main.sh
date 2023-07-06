0057cb879f926ecda7c027c6cc4a32a3df1aa5b3
Facundo Muñoz <facundo.munoz@cirad.fr>
2023-07-06 13:30:47 +0200

    Install VSCodium from debian package repository, rather than from flatpak.
    Flatpak gives issues finding system tools and binaries, due to sandboxing.

--- main.sh
+++ main.sh
@@ -146,7 +146,16 @@ sudo flatpak install gitahead
 sudo flatpak install mattermost
 
 # VSCodium
-sudo flatpak install VSCodium
+## https://vscodium.com/
+## Add the GPG key of the repository:
+sudo wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+## Add the repository:
+sudo echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+## Update then install vscodium (if you want vscodium-insiders, then replace codium by codium-insiders):
+sudo apt update && sudo apt install codium
 
 # Getting Things Gnome!
 # https://wiki.gnome.org/Apps/GTG
